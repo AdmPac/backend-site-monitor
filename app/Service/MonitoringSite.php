a9e2442cf3fa9ab1305482212b23263fc1d30bae
Andrew <for_apb2015@mail.ru>
2025-12-23 21:51:14 +0300

    Класс для мониторинга в целом

---
+++ app/Service/MonitoringSite.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Service;
+
+use App\Models\Site;
+use Illuminate\Support\Facades\Http;
+
+class MonitoringSite
+{
+    public function monitor(Site $site): array
+    {
+        $response = Http::get($site->url);
+        return [
+            // Группа «Здоровье и Статус»
+            'successful' => $response->successful(), // true если статус код 200-299
+            'status' => $response->status(), // статус код
+            'failed' => $response->failed(), // true если статус код не 200-299(>=400)
+            'serverError' => $response->serverError(), // true если статус код 500-599
+            'reason' => $response->reason(), // текстовое описание статуса
+            
+            // Группа «Глубокий сетевой анализ» (Киллер-фичи)
+            'handlerStats' => $response->handlerStats(), // массив с информацией о обработчике запроса
+            'effectiveUri' => $response->effectiveUri(), // URL после всех редиректов
+            'redirect' => $response->redirect(), // массив с информацией о редиректах
+
+            // Группа «Проверка контента и Безопасность»
+            'body' => $response->body(), // весь HTML код
+            'headers' => $response->headers(), // список всех заголовков
+            'cookies' => $response->cookies(), // список всех куков
+        ];
+    }
+}

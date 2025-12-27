f038b22078126f755be228435542661fa3dac35b
Andrew <for_apb2015@mail.ru>
2025-12-27 17:49:43 +0300

    Переименование переменной

--- app/DTO/TimeResponse.php
+++ app/DTO/TimeResponse.php
@@ -14,16 +14,16 @@ readonly class TimeResponse
         public string $ip // IP-адрес сервера
     ) {}
 
-    public static function fromHandlerStats(array $stats): self
+    public static function fromHandlerStats(array $handlerStats): self
     {
         return new self(
-            totalTimeMs: round($stats['total_time'] * 1000, 2), // общее время запроса
-            dnsTimeMs: round($stats['namelookup_time'] * 1000, 2), // время DNS-запроса
-            connectionTimeMs: round($stats['connect_time'] * 1000, 2), // время соединения
-            sslHandshakeMs: round(($stats['appconnect_time'] - $stats['connect_time']) * 1000, 2), // время SSL-handshake
-            ttfbMs: round($stats['starttransfer_time'] * 1000, 2), // время до первого байта
-            downloadSizeBytes: (int) $stats['size_download'], // размер загруженных данных
-            ip: $stats['primary_ip'] ?? '0.0.0.0' // IP-адрес сервера
+            totalTimeMs: round($handlerStats['total_time'] * 1000, 2), // общее время запроса
+            dnsTimeMs: round($handlerStats['namelookup_time'] * 1000, 2), // время DNS-запроса
+            connectionTimeMs: round($handlerStats['connect_time'] * 1000, 2), // время соединения
+            sslHandshakeMs: round(($handlerStats['appconnect_time'] - $handlerStats['connect_time']) * 1000, 2), // время SSL-handshake
+            ttfbMs: round($handlerStats['starttransfer_time'] * 1000, 2), // время до первого байта
+            downloadSizeBytes: (int) $handlerStats['size_download'], // размер загруженных данных
+            ip: $handlerStats['primary_ip'] ?? '0.0.0.0' // IP-адрес сервера
         );
     }
 }

67471c5959b4ef92376f000078b1853ec1d33567
Andrew <for_apb2015@mail.ru>
2025-12-28 10:38:38 +0300

    Добавил SiteService

--- app/Service/SiteService.php
+++ app/Service/SiteService.php
@@ -2,30 +2,18 @@
 
 namespace App\Service;
 
+use App\DTO\MainData;
 use App\Models\Site;
+use App\ValueObjects\DomainName;
 use Illuminate\Support\Facades\Http;
 
 class SiteService
 {
-    public function check(Site $site): array
+    public function getData(Site $site): MainData
     {
-        $response = Http::get($site->url);
-        return [
-            // Группа «Здоровье и Статус»
-            'successful' => $response->successful(), // true если статус код 200-299
-            'status' => $response->status(), // статус код
-            'failed' => $response->failed(), // true если статус код не 200-299(>=400)
-            'serverError' => $response->serverError(), // true если статус код 500-599
-            'reason' => $response->reason(), // текстовое описание статуса
-            
-            // Группа «Глубокий сетевой анализ» (Киллер-фичи)
-            'handlerStats' => $response->handlerStats(), // массив с информацией о обработчике запроса
-            'effectiveUri' => $response->effectiveUri(), // URL после всех редиректов
-            'redirect' => $response->redirect(), // массив с информацией о редиректах
-
-            // Группа «Проверка контента и Безопасность»
-            'headers' => $response->headers(), // список всех заголовков
-            'cookies' => $response->cookies(), // список всех куков
-        ];
+        $domain = new DomainName($site->full_url);
+        $response = Http::get($domain->getFullUrl());
+        $mainData = MainData::createFromHttp($response);
+        return $mainData;
     }
 }

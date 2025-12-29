a764ead64eab3e5dc575052c3fc39b5ed4634937
Andrew <for_apb2015@mail.ru>
2025-12-29 11:26:08 +0300

    Добавил роуты

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,14 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/sites', [SiteController::class, 'create']);
+    Route::get('/sites', [SiteController::class, 'all']);
+    Route::get('/sites/{site}', [SiteController::class, 'get']);
+});
